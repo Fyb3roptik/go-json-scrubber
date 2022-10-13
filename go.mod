9bc610db816601d3c38f14610f58553dd77ad7b1
John Hobbs <john@velvetcache.org>
2022-10-13 16:38:21 -0500

    Add go module and test.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Fyb3roptik/go-json-scrubber
+
+go 1.16
---
+++ scrubber_test.go
@@ -0,0 +1,59 @@
+package jsonscrubber_test
+
+import (
+	"strings"
+	"testing"
+
+	jsonscrubber "github.com/Fyb3roptik/go-json-scrubber"
+)
+
+type User struct {
+	FirstName string   `json:"first_name"`
+	LastName  string   `json:"last_name"`
+	Address   *Address `json:"address"`
+}
+
+type Address struct {
+	Address string `json:"address"`
+	City    string `json:"city"`
+	State   string `json:"state"`
+	Zip     string `json:"zip"`
+}
+
+func TestAddOnly(t *testing.T) {
+	user := &User{
+		FirstName: "Foo",
+		LastName:  "Bar",
+		Address: &Address{
+			City:  "New York",
+			State: "NY",
+		},
+	}
+
+	scrubbedUser := jsonscrubber.AddOnly(user, "first_name", "address").(map[string]interface{})
+	scrubbedUser["address"] = jsonscrubber.AddOnly(user.Address, "city")
+
+	expectedScrubbed := [][]string{{"last_name"}, {"address", "address"}, {"address", "state"}, {"address", "zip"}}
+
+	for _, path := range expectedScrubbed {
+		if fieldExists(scrubbedUser, path) {
+			t.Errorf("unexpected field found %q", strings.Join(path, "."))
+		}
+	}
+}
+
+// test helper, depends on the full object tree being map[string]interface{}
+func fieldExists(object map[string]interface{}, path []string) bool {
+	key := path[0]
+	value, ok := object[key]
+
+	if !ok {
+		return false
+	}
+
+	if len(path) == 1 {
+		return true
+	}
+
+	return fieldExists(value.(map[string]interface{}), append([]string{}, path[1:]...))
+}
